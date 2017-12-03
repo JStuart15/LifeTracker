1d17405846ba20365b62138a35c6f72085922758
jstuart15 <stuart.jeffrey@gmail.com>
2017-12-03 13:42:23 -0600

    create NB project

---
+++ LifeTracker/src/main/java/com/sg/lifetracker/model/User.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.lifetracker.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author jstuart15
+ */
+public class User {
+    private long user_id;
+    private List<Activity> activities;
+    
+}
